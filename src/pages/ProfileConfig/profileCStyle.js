b324068bb554c6943c28973e58bf74f8abfe480e
janson <jansonquiel@gmail.com>
2024-08-30 15:18:44 -0300

    criação e config da page ProfileConfig

---
+++ src/pages/ProfileConfig/profileCStyle.js
@@ -0,0 +1,9 @@
+import { StyleSheet } from 'react-native';
+import { themes } from '@/styles/theme/index';
+
+export default StyleSheet.create({
+  containter: {
+    flex: 1,
+    backgroundColor: themes.colors.white,
+  },
+});
---
+++ src/pages/ProfileConfig/profileConfig.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import styles from './profileCStyle';
+
+function profileConfig() {
+  return <div style={styles.containter}>profileConfig</div>;
+}
+
+export default profileConfig;
