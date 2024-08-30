84da3f06360b6c16e662862147c94b8c29bc2952
janson <jansonquiel@gmail.com>
2024-08-30 15:10:15 -0300

    Adicionando icon no header e TouchableOpacity

--- src/components/Header/header.tsx
+++ src/components/Header/header.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 import styles from './headerStyles';
-import { View, Image } from 'react-native';
+import Icon from 'react-native-vector-icons/EvilIcons';
+import { View, Image, TouchableOpacity } from 'react-native';
 
 const Header = () => {
   return (
     <View style={styles.header}>
-      <View style={styles.iconContainer}></View>
+      <TouchableOpacity>
+        <View style={styles.iconContainer}>
+          <Icon name="bell" size={30} />
+        </View>
+      </TouchableOpacity>
       <View style={styles.logoContainer}>
         <Image source={require('../../assets/images/Logo1.jpg')} style={styles.logo} />
       </View>
-      <View style={styles.profileContainer}>
-        <Image source={require('../../assets/images/Perfil.jpg')} style={styles.profile} />
-      </View>
+      <TouchableOpacity>
+        <View style={styles.profileContainer}>
+          <Image source={require('../../assets/images/Perfil.jpg')} style={styles.profile} />
+        </View>
+      </TouchableOpacity>
     </View>
   );
 };
